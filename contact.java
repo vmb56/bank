fb2ee6cda51e1f476a6807701697d640c78cf166
vmb56 <124532199+vmb56@users.noreply.github.com>
2024-06-24 10:15:23 +0000

    Update contact.java

--- contact.java
+++ contact.java
@@ -1,76 +1,87 @@
-// Classe BankAccount
-// Classe BankAccount
-class BankAccount {
-    protected String account;
-    protected double balance;
 
-    public BankAccount(String account, double balance) {
-        this.account = account;
-        this.balance = balance;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-}
-
-// Classe CheckingAccount (Compte Courant)
-class CheckingAccount extends BankAccount {
-    private double limit;
-
-    public CheckingAccount(String account, double balance, double limit) {
-        super(account, balance);
-        this.limit = limit;
-    }
-
-    public double getLimit() {
-        return limit;
-    }
-
-    public void setLimit(double limit) {
-        this.limit = limit;
-    }
-}
-
-// Classe SavingsAccount (Compte d'épargne)
-class SavingsAccount extends BankAccount {
-    public SavingsAccount(String account, double balance) {
-        super(account, balance);
-    }
-}
-
-// Classe COD (Certificat de Dépôt)
-class COD extends BankAccount {
-    public COD(String account, double balance) {
-        super(account, balance);
-    }
-}
-
-// Classe principale BankManager
+package BankManager;
 public class BankManager {
+
+   
+    static class BankAccount {
+        protected String account;
+        protected double balance;
+
+        public BankAccount(String account, double balance) {
+            this.account = account;
+            this.balance = balance;
+        }
+
+        public String getAccount() {
+            return account;
+        }
+
+        public void setAccount(String account) {
+            this.account = account;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+
+        @Override
+        public String toString() {
+            return "Account: " + account + ", Balance: " + balance;
+        }
+    }
+
+    
+    static class CheckingAccount extends BankAccount {
+        private double limit;
+
+        public CheckingAccount(String account, double balance, double limit) {
+            super(account, balance);
+            this.limit = limit;
+        }
+
+        public double getLimit() {
+            return limit;
+        }
+
+        public void setLimit(double limit) {
+            this.limit = limit;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + ", Limit: " + limit;
+        }
+    }
+
+    
+    static class SavingsAccount extends BankAccount {
+        public SavingsAccount(String account, double balance) {
+            super(account, balance);
+        }
+    }
+
+    
+    static class COD extends BankAccount {
+        public COD(String account, double balance) {
+            super(account, balance);
+        }
+    }
+
     public static void main(String[] args) {
-        // Création d'une instance de CheckingAccount
+        
         CheckingAccount checkingAccount = new CheckingAccount("CHK123", 1500.0, 500.0);
-        System.out.println("Checking Account: " + checkingAccount.getAccount() + ", Balance: " + checkingAccount.getBalance() + ", Limit: " + checkingAccount.getLimit());
+        System.out.println("Checking Account: " + checkingAccount);
 
-        // Création d'une instance de SavingsAccount
+    
         SavingsAccount savingsAccount = new SavingsAccount("SAV456", 3000.0);
-        System.out.println("Savings Account: " + savingsAccount.getAccount() + ", Balance: " + savingsAccount.getBalance());
+        System.out.println("Savings Account: " + savingsAccount);
 
-        // Création d'une instance de COD
+      
         COD cod = new COD("COD789", 10000.0);
-        System.out.println("COD Account: " + cod.getAccount() + ", Balance: " + cod.getBalance());
+        System.out.println("COD Account: " + cod);
     }
 }
